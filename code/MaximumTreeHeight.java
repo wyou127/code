5d134259213516884b88af53254ddfbdbdd63a11
wyou <wyou@walmartlabs.com>
2015-04-03 16:04:36 -0700

    add

--- code/MaximumTreeHeight.java
+++ code/MaximumTreeHeight.java
@@ -5,4 +5,32 @@ package code;
  * Author: <A HREF="mailto:wyou@walmartlabs.com">Wei You</A>
  */
 public class MaximumTreeHeight {
+    private int max;
+    private String maxPath;
+
+    public int maxHeight(TreeNode treeNode, int level) {
+        if (treeNode ==null) {
+            return 0;
+        } else {
+            level = level++;
+            return  Math.max(maxHeight(treeNode.right, level), maxHeight(treeNode.left, level));
+        }
+    }
+
+
+    public void printMaxHeight(TreeNode treeNode, String path) {
+        if (treeNode==null) {
+            return;
+        }
+        path  = path + ""+ treeNode.val;
+        if (treeNode.left == null || treeNode.right == null) {
+            if (max < path.length()) {
+                max = path.length();
+                maxPath = path;
+            }
+        }
+        printMaxHeight(treeNode.left, path);
+        printMaxHeight(treeNode.right, path);
+
+    }
 }
